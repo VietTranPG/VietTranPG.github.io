c232da00246c8f55698eaa8f31debf11f71e4414
Viet.tranhoang <viet.tranhoang@powergatesoftware.com>
2017-03-20 17:05:56 +0700

    a

--- js/map.js
+++ js/map.js
@@ -1,10 +1,12 @@
 $(document).ready(function() {
+  var icon_blue = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
+  var icon_red = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
  	function initMap() {
         var map = new google.maps.Map(document.getElementById('map'), {
           center: {lat: -34.397, lng: 150.644},
           zoom: 13
         });
-        var infoWindow = new google.maps.InfoWindow({map: map});
+        // var infoWindow = new google.maps.InfoWindow({map: map});
         // Try HTML5 geolocation.
         if (navigator.geolocation) {
           navigator.geolocation.getCurrentPosition(function(position) {
@@ -12,9 +14,17 @@ $(document).ready(function() {
               lat: position.coords.latitude,
               lng: position.coords.longitude
             };
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
+            // infoWindow.setPosition(pos);
+            // infoWindow.setContent('Location found.');
             map.setCenter(pos);
+            var marker = new google.maps.Marker({
+                        icon: icon_red,
+                        animation: google.maps.Animation.DROP,
+                        position: new google.maps.LatLng(pos.lat,pos.lng),
+                        map: map,
+                        title: 'Nơi bắt đầu'
+                    });
+
           }, function() {
             handleLocationError(true, infoWindow, map.getCenter());
           });
