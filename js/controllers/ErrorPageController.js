8f6e29f164603a2909e5e19be87ab92f050f0897
Viet.tranhoang <viet.tranhoang@powergatesoftware.com>
2017-06-07 17:36:16 +0700

    sdvgsdf

--- js/controllers/ErrorPageController.js
+++ js/controllers/ErrorPageController.js
@@ -1,5 +1,6 @@
-﻿angular.module('LocalOS').controller('ErrorPageController', function ($rootScope, $scope, $http, $timeout, $state,$filter) {
+﻿angular.module('LocalOS').controller('ErrorPageController', function ($rootScope, $scope, $http, $timeout, $state,$filter,timeAgo) {
    $scope.currentDate=$filter('date')(new Date(), 'yyyy/M/d HH:mm:ss');
    $scope.timezone = ( new Date( $scope.currentDate).getTimezoneOffset()/ -60);
    $scope.changeTime =  $filter('date')(new Date($scope.currentDate ), 'yyyy/M/d HH:mm:ss','UTC');
+   $scope.changeTime2 =  $filter('date')(new Date($scope.currentDate + " UTC" ), 'yyyy/M/d HH:mm:ss',timezone);
 });
