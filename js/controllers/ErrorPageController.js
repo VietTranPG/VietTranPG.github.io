2baca8937c4ea4f43f64d3a9714f89a7db48248f
Viet.tranhoang <viet.tranhoang@powergatesoftware.com>
2017-06-07 17:16:25 +0700

    123123

--- js/controllers/ErrorPageController.js
+++ js/controllers/ErrorPageController.js
@@ -1,3 +1,5 @@
 ﻿angular.module('LocalOS').controller('ErrorPageController', function ($rootScope, $scope, $http, $timeout, $state,$filter) {
-   $scope.date = new Date();
+   $scope.currentDate = new Date();
+   $scope.timezone = ( $scope.currentDate.getTimezoneOffset() / -60);
+   $scope.changeTime =  $filter('date')(new Date(currentDate + " UTC"), 'yyyy/M/d HH:mm:ss');
 });
