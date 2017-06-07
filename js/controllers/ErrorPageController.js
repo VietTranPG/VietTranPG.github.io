56348341f1b402cb7cadf4e0b31c30d63a925cb9
Viet.tranhoang <viet.tranhoang@powergatesoftware.com>
2017-06-07 17:11:07 +0700

    â

--- js/controllers/ErrorPageController.js
+++ js/controllers/ErrorPageController.js
@@ -1,3 +1,3 @@
-﻿angular.module('LocalOS').controller('ErrorPageController', function ($rootScope, $scope, $http, $timeout, $state) {
-   
+﻿angular.module('LocalOS').controller('ErrorPageController', function ($rootScope, $scope, $http, $timeout, $state,filter) {
+   $scope.date = new Date();
 });
