284402911823a3c18fffd1cc07e9572db7dd9833
Viet.tranhoang <viet.tranhoang@powergatesoftware.com>
2017-06-07 17:37:04 +0700

    sadsda

--- js/controllers/ErrorPageController.js
+++ js/controllers/ErrorPageController.js
@@ -2,5 +2,5 @@
    $scope.currentDate=$filter('date')(new Date(), 'yyyy/M/d HH:mm:ss');
    $scope.timezone = ( new Date( $scope.currentDate).getTimezoneOffset()/ -60);
    $scope.changeTime =  $filter('date')(new Date($scope.currentDate ), 'yyyy/M/d HH:mm:ss','UTC');
-   $scope.changeTime2 =  $filter('date')(new Date($scope.currentDate + " UTC" ), 'yyyy/M/d HH:mm:ss',timezone);
+   $scope.changeTime2 =  $filter('date')(new Date($scope.currentDate + " UTC" ), 'yyyy/M/d HH:mm:ss',$scope.timezone);
 });
