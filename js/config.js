83da1f4493132e9c8d9ce932789b7097e80834a4
viet <viet.tranhoang@powergatesoftware.com>
2017-07-31 22:32:19 +0700

    wedding

--- js/config.js
+++
@@ -1,6 +0,0 @@
-﻿const SERVER_API = "http://api.qa.localhealth.uat2.pgtest.co:8080/";
-const STATUS_SUCCESS = "Success";
-const STATUS_ERROR = "Error";
-const ACCESS_DENIED = "Access is denied";
-const VALUE_OF_DOLLAR = "RAW";
-const VALUE_OF_PERCENTAGE = "PERCENT";
--- js/controllers/HeaderController.js
+++
@@ -1,6 +0,0 @@
-﻿
-angular.module('LocalOS').controller('HeaderController', function ($rootScope, $scope, $http, $timeout, $state, logoutService) {
-    $scope.logout = function (isButton) {        
-        logoutService.logout(isButton);
-    }
-});
