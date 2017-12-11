9f6381566584a62408428477b277cb15de5c6d78
tranhoangviet2412 <viet.tranhoang@powergatesoftware.com>
2017-12-11 16:21:10 +0700

    tesst

--- app/account/createdAccountController.js
+++
@@ -1,5 +0,0 @@
-app.controller('createdAccountCtrl', function($ionicPlatform, $stateParams, $rootScope, $scope, $http, $window, $state){
-    var vm = this;
-    vm.name = $stateParams.name;
-    console.log(vm.name);
-})
